09a56bbac9dbfb756804c2b9d914779750f2a3ec
Stanley <angstanley68@gmail.com>
2020-10-11 00:59:18 +0700

    feat: Add auth function

---
+++ adapters/util.js
@@ -0,0 +1,17 @@
+module.exports = {
+    isObject: (obj) => {
+        return (obj instanceof Object) && !(Array.isArray(obj))
+    },
+
+    isString: (str) => {
+        return (typeof str === 'string')
+    },
+
+    isUndefined: (thing) => {
+        return (typeof thing === 'undefined')
+    },
+
+    isBlank: (obj) => {
+        return (typeof obj === 'undefined') || (obj === null) || (obj === '')
+    }
+}
