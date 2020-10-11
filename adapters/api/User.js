03f5accb13d1d11fbea2b92c201821175cb5dfdf
Stanley <angstanley68@gmail.com>
2020-10-12 02:25:51 +0700

    fix(api): Remove request body from getUserLoginDetail and getBinusianPhoto
    These endpoints doesn't actually need request body from client.

--- adapters/api/User.js
+++ adapters/api/User.js
@@ -16,55 +16,20 @@ class User {
         })
   }
 
-  getUserLoginDetail(binusianId) {
+  getUserLoginDetail() {
     const API_PATH = '/api/BinusMobile/Student/V1/Login/Login/GetStudentLoginDetail'
 
-    if (isBlank(binusianId)) {
-      throw new Error('Please set valid `binusianId`')
-    }
-
-    return this.client.$axios.post(API_PATH,
-        this.client.$request.formdata({
-          BinusianID: binusianId
-        }, []),
-        {
-            headers: this._headers()
-        })
+    return this.client.$axios.post(API_PATH, {}, {headers: this._headers()})
         .then(response => response.data)
         .catch(err => {
           throw new Error(err.response.data.error_description)
         })
   }
 
-  getBinusianPhoto(binusianId, acadCareer, emplid, roleID) {
+  getBinusianPhoto() {
     const API_PATH = '/api/Base/V1/BinusianPhoto/BinusianPhoto/get'
 
-    if (isBlank(binusianId)) {
-      throw new Error('Please set valid `binusianId`')
-    }
-
-    if (isBlank(acadCareer)) {
-      throw new Error('Please set valid `acadCareer`')
-    }
-
-    if (isBlank(emplid)) {
-      throw new Error('Please set valid `emplid`')
-    }
-
-    if (isBlank(roleID)) {
-      throw new Error('Please set valid `roleID`')
-    }
-
-    return this.client.$axios.post(API_PATH,
-        this.client.$request.formdata({
-          BinusianID: binusianId,
-          acadCareer,
-          emplid,
-          roleID
-        }),
-        {
-            headers: this._headers()
-        })
+    return this.client.$axios.post(API_PATH, {}, {headers: this._headers()})
         .then(response => response.data)
         .catch(err => {
           throw new Error(err.response.data.error_description)
